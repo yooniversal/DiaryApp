0369aa52a4ad3b78230da0e76cec0cc32827f18b
yooniversal <iteratively@naver.com>
2020-12-16 16:29:53 +0900

    로그인 화면 추가
    로그인 기능은 미구현
    화면만 xml을 이용해서 추가

---
+++ app/src/main/java/com/example/newProject/LoginPage.kt
@@ -0,0 +1,14 @@
+package com.example.newProject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+    }
+
+    btn_login.
+}
--- app/src/main/java/com/example/newProject/MainActivity.kt
+++ app/src/main/java/com/example/newProject/MainActivity.kt
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.content.Intent;
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.concurrent.schedule
+
 
 class MainActivity : AppCompatActivity() {
 
-    val TAG: String = "로그"
+    val TAG: String = "Splash Screen 로그"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // R(res)/layout/activity_main.xml 연결
         setContentView(R.layout.activity_main)
+
+        Timer().schedule(2000){
+            //JUST DELAY 2 SEC
+        }
+
+        startActivity(Intent(this, LoginPage::class.java))
+        finish()
         
         Log.d(TAG, "MainActivity - onCreate() called")
     }
