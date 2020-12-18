8cc9b96b9d3ac0d39851a336b3faec9ab6fba269
yooniversal <iteratively@naver.com>
2020-12-18 14:57:49 +0900

    로그인 화면
    ID, PW 인덱스 추가
    팝업 테스트

--- app/src/main/java/com/example/newProject/LoginPage.kt
+++ app/src/main/java/com/example/newProject/LoginPage.kt
@@ -3,6 +3,7 @@ package com.example.newProject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.newProject.databinding.ActivityLoginPageBinding
 
 class LoginPage : AppCompatActivity() {
@@ -13,7 +14,13 @@ class LoginPage : AppCompatActivity() {
         setContentView(R.layout.activity_login_page)
         val binding = ActivityLoginPageBinding.inflate(layoutInflater)
         binding.btnLogin.setOnClickListener(){ //로그인 버튼 눌렀을 때 다음 상태 만들기
-            startActivity(Intent(this, LoginPage::class.java))
+            var id = binding.EditID.toString()
+            var pw = binding.EditPW.toString()
+
+            var dialog = AlertDialog.Builder(this@LoginPage)
+            dialog.setTitle("알람")
+            dialog.setMessage("id : " + id)
+            dialog.show()
         }
         setContentView(binding.root)
     }
